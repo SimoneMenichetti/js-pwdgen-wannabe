de4f4119100c9c12f0bcaf0bd5a62673b509ba3e
Menichetti Simone <simon3menichetti95@gmail.com>
2024-06-03 15:40:51 +0200

    ok funzionamento console

--- JS/main.js
+++ JS/main.js
@@ -1,15 +1,22 @@
 // Chiedi all’utente il suo nome,
- 
-let userName = prompt("inserire nome utente");
+    let userName = prompt("inserire nome utente");
 
 //  poi chiedi il suo cognome,
 
- let surName = prompt("inserire cognome utente");
+    let surName = prompt("inserire cognome utente");
 
 
 //   poi chiedi il suo colore preferito
 
-  let favoriteColor = prompt("inserire colore preferito utente");
+    let favoriteColor = prompt("inserire colore preferito utente");
+
+//   concatenamento stringa combinata
+
+    let risultato = userName + surName + favoriteColor + "23";
+
+//  output console risultato concatenamento
+    console.log("i dati anagrafici utente più colore preferito ed in numero equivale a: ", risultato);
 
 
 // Infine scrivi sulla pagina nomecognomecolorepreferito23
+

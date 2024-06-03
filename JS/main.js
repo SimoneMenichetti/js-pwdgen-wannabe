c48ae08493f6ff14e0b1a2ea89ff91d54ab38aed
Menichetti Simone <simon3menichetti95@gmail.com>
2024-06-03 15:26:08 +0200

    inizio concatenamento risultato nome + cognome + colore + 23

--- JS/main.js
+++ JS/main.js
@@ -1,2 +1,15 @@
-// Chiedi all’utente il suo nome, poi chiedi il suo cognome, poi chiedi il suo colore preferito
+// Chiedi all’utente il suo nome,
+ 
+let userName = prompt("inserire nome utente");
+
+//  poi chiedi il suo cognome,
+
+ let surName = prompt("inserire cognome utente");
+
+
+//   poi chiedi il suo colore preferito
+
+  let favoriteColor = prompt("inserire colore preferito utente");
+
+
 // Infine scrivi sulla pagina nomecognomecolorepreferito23
